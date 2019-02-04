77a4b7c3bbbb9abc52caf89bb4ac144f5df3089a
nve3pd <ic151206@edu.okinawa-ct.ac.jp>
2019-02-05 03:31:50 +0900

    feat: initial commit

---
+++ calculator/Caluculator.java
@@ -0,0 +1,9 @@
+import java.awt.*;
+
+public class Caluculator {
+    public static void main(String[] args) {
+       Frame f = new Frame();
+       f.setSize(300, 200);
+       f.setVisible(true);
+    }
+}
