b95f1e60b991b0d3908dc0b3d9e2e927d7b67269
ucpr <hoge.uchihara@gmail.com>
2019-08-01 22:51:38 +0900

    initial commit

---
+++ auth0_test02/app.py
@@ -0,0 +1,118 @@
+import json
+import os
+from functools import wraps
+
+from six.moves.urllib.request import urlopen
+import responder
+from jose import jwt
+
+api = responder.API()
+
+
+AUTH0_DOMAIN = os.getenv("AUTH0_DOMAIN")
+API_AUDIENCE = os.getenv("API_AUDIENCE")
+ALGORITHMS = ["RS256"]
+
+
+class AuthError(Exception):
+    ## AuthErrorを定義しているが,
+    ## responderでerror_handleてきなのができないっぽいのでtry-exceptしないと行けなさそう
+    def __init__(self, error, status_code):
+        self.error = error
+        self.status_code = status_code
+
+
+def get_token_auth_header(headers):
+    """Obtains the access token from the Authorization Header
+    """
+    auth = headers.get("Authorization", None)
+    if not auth:
+        raise AuthError({"code": "authorization_header_missing",
+                        "description":
+                            "Authorization header is expected"}, 401)
+
+    parts = auth.split()
+
+    if parts[0].lower() != "bearer":
+        raise AuthError({"code": "invalid_header",
+                        "description":
+                            "Authorization header must start with"
+                            " Bearer"}, 401)
+    elif len(parts) == 1:
+        raise AuthError({"code": "invalid_header",
+                        "description": "Token not found"}, 401)
+    elif len(parts) > 2:
+        raise AuthError({"code": "invalid_header",
+                        "description":
+                            "Authorization header must be"
+                            " Bearer token"}, 401)
+
+    token = parts[1]
+    return token
+
+def requires_auth(f):
+    """Determines if the access token is valid
+    """
+    @wraps(f)
+    def decorated(*args, **kwargs):
+        try:
+            ## 2回呼び出しされて, 2回目のリクエストで領域外参照をするためreturn  TODO: ここいい感じにする
+            token = get_token_auth_header(args[0].headers)
+        except:
+            return
+        jsonurl = urlopen("https://"+AUTH0_DOMAIN+"/.well-known/jwks.json")
+        jwks = json.loads(jsonurl.read())
+        unverified_header = jwt.get_unverified_header(token)
+        rsa_key = {}
+        for key in jwks["keys"]:
+            if key["kid"] == unverified_header["kid"]:
+                rsa_key = {
+                    "kty": key["kty"],
+                    "kid": key["kid"],
+                    "use": key["use"],
+                    "n": key["n"],
+                    "e": key["e"]
+                }
+        if rsa_key:
+            try:
+                payload = jwt.decode(
+                    token,
+                    rsa_key,
+                    algorithms=ALGORITHMS,
+                    audience=API_AUDIENCE,
+                    issuer="https://"+AUTH0_DOMAIN+"/"
+                )
+            except jwt.ExpiredSignatureError:
+                raise AuthError({"code": "token_expired",
+                                "description": "token is expired"}, 401)
+            except jwt.JWTClaimsError:
+                raise AuthError({"code": "invalid_claims",
+                                "description":
+                                    "incorrect claims,"
+                                    "please check the audience and issuer"}, 401)
+            except Exception:
+                raise AuthError({"code": "invalid_header",
+                                "description":
+                                    "Unable to parse authentication"
+                                    " token."}, 400)
+
+                #_app_ctx_stack.top.current_user = payload  TODO: flaskのだけどresponderでどうするかわからん
+            return f(*args, **kwargs)
+        raise AuthError({"code": "invalid_header",
+                        "description": "Unable to find appropriate key"}, 400)
+    return decorated
+
+
+
+@api.route("/ping")
+def ping(req, resp):
+    resp.text = "All Good."
+
+
+@api.route("/secure/ping")
+@requires_auth
+def secure_ping(req, resp):
+    resp.text = "Perfect."
+
+
+api.run()
